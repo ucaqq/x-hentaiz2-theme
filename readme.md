1e914d39d62a8d421611d24ec8da026f24878b2f
Anonymous <anonymous@gmail.com>
2026-02-18 04:26:17 +0000

    init cp from another theme

---
+++ readme.md
@@ -0,0 +1,8 @@
+# Clone from xem.phimsexmoi123.com
+
+# Meta Keys
+
+| Key | Desc |
+|-----|------|
+| footer-text | Support raw html |
+| neon-theme-color | Default: #e69e14 |
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["/css/**/*", "./views/**/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
